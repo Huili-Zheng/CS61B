cef77e99037ac76df4748345af3207edd480a08f
Huili Zheng <hz2710@cumc.columbia.edu>
2022-12-09 16:59:01 -0500

    get collatz back

--- lab01/Collatz.java
+++ lab01/Collatz.java
@@ -3,14 +3,16 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Returns the nextNumber in a Collatz sequence. */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        // TODO: Fill in this method.
+        if (n == 1) {
             return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        }
+        if (n % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
+            return 3 * n + 1;
         }
     }
 
