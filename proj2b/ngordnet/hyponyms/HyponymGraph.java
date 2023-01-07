0eed8c84925ae15a8a137badf2f70ce4e9ee9d09
Huili Zheng <hz2710@cumc.columbia.edu>
2023-01-07 16:01:21 -0500

    fix the graph without V by ArrayList

--- proj2b/ngordnet/hyponyms/HyponymGraph.java
+++ proj2b/ngordnet/hyponyms/HyponymGraph.java
@@ -1,42 +1,47 @@
 package ngordnet.hyponyms;
 
-
-import edu.princeton.cs.algs4.In;
-
 import java.util.*;
 
 public class HyponymGraph {
-
-    private final int V;
-    private List<Integer>[] adj;
+    // a list to store the vertices of the graph
+    private List<Integer> vertices;
+    // an adjacency list to store the edges of the graph
+    private List<List<Integer>> adj;
 
 
     /**
      * Create empty graph
      */
-    public HyponymGraph(int V) {
-        this.V = V;
-        adj = (List<Integer>[]) new ArrayList[V];
-        for (int v = 0; v < V; v++) {
-            adj[v] = new ArrayList<Integer>();
-        }
+    public HyponymGraph() {
+        vertices = new ArrayList<>();
+        adj = new ArrayList<>();
     }
 
+    // add a vertex to the graph
+    public void addVertex(int v) {
+        vertices.add(v);
+        // add an empty list to store the edges of the vertex
+        adj.add(new ArrayList<>());
+    }
+
+    // add an edge between two vertices
     public void addEdge(int v, int w) {
-        adj[v].add(w);
+        adj.get(v).add(w);
     }
 
-
-    public Iterable<Integer> adj(int v) {
-        return adj[v];
+    // get the adj list of a vertex
+    public List<Integer> getAdj(int v) {
+        return adj.get(v);
     }
 
+    // check the existence of the edge between two vertices
     public boolean hasEdge(int v, int w) {
-        return adj[v].contains(w);
+        return adj.get(v).contains(w);
     }
 
+    // return the # of vertices
     public int size() {
-        return V;
+        return vertices.size();
     }
 
 }
