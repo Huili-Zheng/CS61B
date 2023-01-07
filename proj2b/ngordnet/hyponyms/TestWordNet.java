63123687b3f14878958c458e9ed5b97bacf84cdd
Huili Zheng <hz2710@cumc.columbia.edu>
2023-01-07 15:57:56 -0500

    TestWordNet.TestGetIntersection

--- proj2b/ngordnet/hyponyms/TestWordNet.java
+++ proj2b/ngordnet/hyponyms/TestWordNet.java
@@ -3,20 +3,22 @@ package ngordnet.hyponyms;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Set;
+
 
 import static org.junit.Assert.*;
 
 public class TestWordNet {
-    @Test
-    public void TestWordNet() {
-        // create a wordNet to index the data
-        WordNet wn = new WordNet("./data/wordnet/synsets16.txt", "./data/wordnet/hyponyms16.txt");
 
-        // test getSortedHyponyms()
-        assertEquals(Set.of("alteration", "change", "demotion", "increase", "jump", "leap", "modification", "saltation", "transition", "variation"), wn.getSortedHyponyms("change"));
+    public WordNet createWordNet() {
+        // create a wordNet to index the data
+        return new WordNet("./data/wordnet/synsets16.txt", "./data/wordnet/hyponyms16.txt");
+    }
+    @Test
+    public void TestGetIntersection() {
+        // create a wordNet to index the data
+        WordNet wn = createWordNet();
 
         // test getIntersection()
-        assertEquals(Set.of("alteration", "change", "increase", "jump", "leap", "modification", "saltation", "transition"), wn.getIntersection(Arrays.asList("change", "occurrence")));
+        assertEquals(Arrays.asList("alteration", "change", "increase", "jump", "leap", "modification", "saltation", "transition"), wn.getIntersection(Arrays.asList("change", "occurrence")));
     }
 }
