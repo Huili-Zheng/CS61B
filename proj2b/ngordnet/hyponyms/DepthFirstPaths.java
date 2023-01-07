276cc02157d36857ac16e361da7ad6f33262b8ac
Huili Zheng <hz2710@cumc.columbia.edu>
2023-01-06 23:04:59 -0500

    DepthFirstPaths class add connectedComponent(v) to get all the nodes that v can arrive

--- proj2b/ngordnet/hyponyms/DepthFirstPaths.java
+++ proj2b/ngordnet/hyponyms/DepthFirstPaths.java
@@ -1,14 +1,17 @@
 package ngordnet.hyponyms;
 
+import java.util.HashSet;
 import java.util.Stack;
 
 public class DepthFirstPaths {
     private boolean[] marked;
     private int[] edgeTo;
     private int s;
+    private HyponymGraph graph;
     public DepthFirstPaths(HyponymGraph G, int s) {
         marked = new boolean[G.size()] ;
         edgeTo = new int[G.size()];
+        graph = G;
         dfs(G, s);
     }
 
@@ -31,14 +34,33 @@ public class DepthFirstPaths {
             return null;
         }
         Stack<Integer> path = new Stack<>();
-        for (int x = v; x!=v; x = edgeTo[x]) {
+        for (int x = v; x!=s; x = edgeTo[x]) {
             path.push(x);
         }
         path.push(s);
         return path;
     }
 
-
-
-
+    public HashSet<Integer> connectedComponent(int v) {
+        // create a list to store the connected component
+        HashSet<Integer> component = new HashSet<>();
+        // mark all the vertices as not visited
+        boolean[] visited = new boolean[graph.size()];
+        // create a stack for DFS
+        Stack<Integer> stack = new Stack<>();
+        // do DFS for vertex v
+        component.add(v);
+        stack.push(v);
+        while (!stack.isEmpty()) {
+            int u = stack.pop();
+            for (int w : graph.adj(u)) {
+                if (!visited[w]) {
+                    visited[w] = true;
+                    stack.push(w);
+                    component.add(w);
+                }
+            }
+        }
+        return component;
+    }
 }
