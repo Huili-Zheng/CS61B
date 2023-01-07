c85e87bd8af2eb0991cc0edc87395ac6c60e4b9c
Huili Zheng <hz2710@cumc.columbia.edu>
2023-01-07 14:10:54 -0500

    // test getIntersection()

--- proj2b/ngordnet/hyponyms/TestWordNet.java
+++ proj2b/ngordnet/hyponyms/TestWordNet.java
@@ -2,6 +2,7 @@ package ngordnet.hyponyms;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Set;
 
 import static org.junit.Assert.*;
@@ -9,7 +10,13 @@ import static org.junit.Assert.*;
 public class TestWordNet {
     @Test
     public void TestWordNet() {
-        WordNet wn = new WordNet("./data/wordnet/synsets11.txt", "./data/wordnet/hyponyms11.txt");
-        assertEquals(Set.of("antihistamine", "actifed"), wn.getSortedHyponyms("antihistamine"));
+        // create a wordNet to index the data
+        WordNet wn = new WordNet("./data/wordnet/synsets16.txt", "./data/wordnet/hyponyms16.txt");
+
+        // test getSortedHyponyms()
+        assertEquals(Set.of("alteration", "change", "demotion", "increase", "jump", "leap", "modification", "saltation", "transition", "variation"), wn.getSortedHyponyms("change"));
+
+        // test getIntersection()
+        assertEquals(Set.of("alteration", "change", "increase", "jump", "leap", "modification", "saltation", "transition"), wn.getIntersection(Arrays.asList("change", "occurrence")));
     }
 }
