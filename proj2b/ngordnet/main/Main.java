213192e66a3d7ea6eeffede486e97f0752fe10aa
Huili Zheng <hz2710@cumc.columbia.edu>
2023-01-07 14:09:28 -0500

    workable Main with known V

--- proj2b/ngordnet/main/Main.java
+++ proj2b/ngordnet/main/Main.java
@@ -1,6 +1,7 @@
 package ngordnet.main;
 
 import ngordnet.hugbrowsermagic.HugNgordnetServer;
+import ngordnet.hyponyms.WordNet;
 import ngordnet.ngrams.NGramMap;
 
 public class Main {
@@ -9,13 +10,16 @@ public class Main {
         String wordFile = "./data/ngrams/top_49887_words.csv";
         String countFile = "./data/ngrams/total_counts.csv";
 
-        String synsetFile = "./data/wordnet/synsets.txt";
-        String hyponymFile = "./data/wordnet/hyponyms.txt";
+        String synsetFile = "./data/wordnet/synsets16.txt";
+        String hyponymFile = "./data/wordnet/hyponyms16.txt";
 
         NGramMap ngm = new NGramMap(wordFile, countFile);
+
 
         hns.startUp();
         hns.register("history", new HistoryHandler(ngm));
         hns.register("historytext", new HistoryTextHandler(ngm));
+        WordNet wn = new WordNet(synsetFile, hyponymFile);
+        hns.register("hyponyms", new HyponymsHandler(wn));
     }
 }
