98a91320a5cb1e94fe4d1b3a3a63421561466f14
Huili Zheng <hz2710@cumc.columbia.edu>
2023-01-07 16:00:01 -0500

    source vertex

--- proj2b/ngordnet/hyponyms/DepthFirstPaths.java
+++ proj2b/ngordnet/hyponyms/DepthFirstPaths.java
@@ -4,20 +4,26 @@ import java.util.HashSet;
 import java.util.Stack;
 
 public class DepthFirstPaths {
+    // a boolean array to store the record of visited nodes
     private boolean[] marked;
+    // an array to store the edge-to information
     private int[] edgeTo;
+    // the source vertex
     private int s;
+    // the source graph
     private HyponymGraph graph;
     public DepthFirstPaths(HyponymGraph G, int s) {
         marked = new boolean[G.size()] ;
         edgeTo = new int[G.size()];
+        this.s = s;
         graph = G;
         dfs(G, s);
     }
 
+    // depth-first search from a given source vertex
     private void dfs(HyponymGraph G, int v) {
         marked[v] = true;
-        for (int w : G.adj(v)) {
+        for (int w : G.getAdj(v)) {
             if (!marked[w]) {
                 edgeTo[w] = v;
                 dfs(G, w);
@@ -25,10 +31,12 @@ public class DepthFirstPaths {
         }
     }
 
+    // check if there is a path from source vertex to v given vertex
     public boolean hasPathTo(int v) {
         return marked[v];
     }
 
+    // get the path from source to v
     public Iterable<Integer> pathTo(int v) {
         if (!hasPathTo(v)) {
             return null;
@@ -41,6 +49,7 @@ public class DepthFirstPaths {
         return path;
     }
 
+    // get all the nodes that the source can arrive
     public HashSet<Integer> connectedComponent(int v) {
         // create a list to store the connected component
         HashSet<Integer> component = new HashSet<>();
@@ -53,7 +62,7 @@ public class DepthFirstPaths {
         stack.push(v);
         while (!stack.isEmpty()) {
             int u = stack.pop();
-            for (int w : graph.adj(u)) {
+            for (int w : graph.getAdj(u)) {
                 if (!visited[w]) {
                     visited[w] = true;
                     stack.push(w);
