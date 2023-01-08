c589f8dc8151bd37d162e97ce7892d86c279e171
Huili Zheng <hz2710@cumc.columbia.edu>
2023-01-08 12:09:14 -0500

    add two registers

--- proj2b/ngordnet/main/Main.java
+++ proj2b/ngordnet/main/Main.java
@@ -14,12 +14,14 @@ public class Main {
         String hyponymFile = "./data/wordnet/hyponyms.txt";
 
         NGramMap ngm = new NGramMap(wordFile, countFile);
-
+        WordNet wn = new WordNet(synsetFile, hyponymFile);
 
         hns.startUp();
         hns.register("history", new HistoryHandler(ngm));
         hns.register("historytext", new HistoryTextHandler(ngm));
-        WordNet wn = new WordNet(synsetFile, hyponymFile);
+
         hns.register("hyponyms", new HyponymsHandler(wn, ngm));
+        hns.register("hypohist", new HypohistHandler(wn, ngm));
+        hns.register("hypohisttext", new HypohistTextHandler(wn, ngm));
     }
 }
