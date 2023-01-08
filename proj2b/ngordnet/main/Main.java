e5721e00a2b35b7715f38dd4f099df9e88452e19
Huili Zheng <hz2710@cumc.columbia.edu>
2023-01-07 21:16:02 -0500

    workable on large data set

--- proj2b/ngordnet/main/Main.java
+++ proj2b/ngordnet/main/Main.java
@@ -10,8 +10,8 @@ public class Main {
         String wordFile = "./data/ngrams/top_49887_words.csv";
         String countFile = "./data/ngrams/total_counts.csv";
 
-        String synsetFile = "./data/wordnet/synsets16.txt";
-        String hyponymFile = "./data/wordnet/hyponyms16.txt";
+        String synsetFile = "./data/wordnet/synsets.txt";
+        String hyponymFile = "./data/wordnet/hyponyms.txt";
 
         NGramMap ngm = new NGramMap(wordFile, countFile);
 
@@ -20,6 +20,6 @@ public class Main {
         hns.register("history", new HistoryHandler(ngm));
         hns.register("historytext", new HistoryTextHandler(ngm));
         WordNet wn = new WordNet(synsetFile, hyponymFile);
-        hns.register("hyponyms", new HyponymsHandler(wn));
+        hns.register("hyponyms", new HyponymsHandler(wn, ngm));
     }
 }
